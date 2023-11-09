fd8d7a0822dde7d7c47af6a520c90196fe978e2a
papihoudini <ochiengcliff.co@gmail.com>
2023-11-09 17:50:55 -0500

    Session authentication

--- 0x02-Session_authentication/api/v1/app.py
+++ 0x02-Session_authentication/api/v1/app.py
@@ -64,8 +64,9 @@ def authenticate_user():
         ]
         if auth.require_auth(request.path, excluded_paths):
             auth_header = auth.authorization_header(request)
+            sess_cookie = auth.session_cookie(request)
             user = auth.current_user(request)
-            if auth_header is None:
+            if auth_header is None and sess_cookie is None:
                 abort(401)
             if user is None:
                 abort(403)
