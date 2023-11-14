449845ca4df30f4f62cc49ca8d99f315a99a078b
papihoudini <ochiengcliff.co@gmail.com>
2023-11-14 13:03:28 -0500

    Register user

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -22,4 +22,31 @@ def _hash_password(password: str) -> bytes:
     Returns:
         bytes: hashed password
     """
-    return hashpw(password.encode('utf-8'), gensalt())
+    return hashpw(password.encode("utf-8"), gensalt())
+
+
+class Auth:
+    """
+    Auth class to interact with the authentication database
+    """
+
+    def __init__(self):
+        self._db = DB()
+
+    def register_user(self, email: str, password: str) -> User:
+        """
+        Registers new users to database
+        Args:
+            email (str): user email
+            password (str): user password
+        Returns:
+            User: Newly created user
+        """
+        try:
+            user = self._db.find_user_by(email=email)
+            if user:
+                raise ValueError(f"User {email} already exists")
+        except NoResultFound:
+            hashed_password = _hash_password(password)
+            new_user = self._db.add_user(email, hashed_password)
+        return new_user
