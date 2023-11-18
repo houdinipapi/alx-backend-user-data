8635f504ab085109cdb9d7b075b2106bcbf449e6
papihoudini <ochiengcliff.co@gmail.com>
2023-11-18 09:18:21 -0500

    Log in

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -39,5 +39,25 @@ def register_user() -> Tuple[str, int]:
     return jsonify(response)
 
 
+@app.route('/sessions', methods=['POST'], strict_slashes=False)
+def login() -> str:
+    """
+    Implements login functionality
+    Returns:
+        str: JSON payload with session_id
+    """
+    email = request.form.get('email')
+    password = request.form.get('password')
+    valid_login = AUTH.valid_login(email, password)
+
+    if not valid_login:
+        abort(401)
+
+    session_id = AUTH.create_session(email)
+    response = jsonify({'email': email, 'message': 'logged in'})
+    response.set_cookie('session_id', session_id)
+    return response
+
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port="5000")
