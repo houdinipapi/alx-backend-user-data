931e52104a9194a6e4891550315475551187fbe3
papihoudini <ochiengcliff.co@gmail.com>
2023-11-18 13:43:01 -0500

    User authenticaton service

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -118,3 +118,45 @@ class Auth:
         """
         self._db.update_user(user_id, session_id=None)
         return None
+
+    def get_reset_password_token(self, email: str) -> str:
+        """
+        Generate token for password
+        Args:
+            email (str): user email to send token
+        Returns:
+            str: generated token
+        """
+        try:
+            user = self._db.find_user_by(email=email)
+        except (NoResultFound, InvalidRequestError):
+            raise ValueError
+
+        user.reset_token = _generate_uuid()
+        return user.reset_token
+
+    def update_password(self, reset_token: str, password: str) -> None:
+        """
+        Update password
+
+        Args:
+            reset_token (str): user reset token
+            password (str): new password
+
+        Raises:
+            ValueError: if user can't be found with reset_token
+
+        Returns: None
+        """
+        try:
+            user = self._db.find_user_by(reset_token=reset_token)
+        except Exception:
+            raise ValueError
+
+        hashed_password = _hash_password(password)
+        self._db.update_user(
+            user.id,
+            hashed_password=hashed_password,
+            reset_token=None
+        )
+        return None
