46349c586ded3dd3ac4de06731edf69badb44b57
papihoudini <ochiengcliff.co@gmail.com>
2023-11-18 09:15:10 -0500

    Get Session ID

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -88,7 +88,7 @@ class Auth:
         Returns:
             str: session id
         """
-        
+
         try:
             user = self._db.find_user_by(email=email)
             session_id = _generate_uuid()
