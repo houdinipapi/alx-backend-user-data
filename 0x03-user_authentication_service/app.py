13b50c1e2d7acdb47a822f2a9542bc2aa08b3a4a
papihoudini <ochiengcliff.co@gmail.com>
2023-11-18 08:57:53 -0500

    Register user

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 """"
-basic flask app
+Basic flask app
 """
 
 from typing import Tuple, Union
@@ -20,6 +20,23 @@ def index() -> str:
         str: JSON payload
     """
     return jsonify({"message": "Bienvenue"})
+
+
+@app.route("/users", methods=["POST"])
+def register_user() -> Tuple[str, int]:
+    """
+    Registers a new user if it does not exist.
+    """
+    email = request.form.get("email")
+    password = request.form.get("password")
+
+    try:
+        user = AUTH.register_user(email, password)
+    except ValueError:
+        return jsonify({"message": "email already registered"}), 400
+
+    response = {"email": email, "message": "user created"}
+    return jsonify(response)
 
 
 if __name__ == "__main__":
